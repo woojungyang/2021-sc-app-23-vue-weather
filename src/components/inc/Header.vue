8b27f1bbc83ef856b9d37cba61df312c9d8d49cf
woojungyang <blossom1113@nate.com>
2021-10-07 16:39:52 +0900

    header-design complete

---
+++ src/components/inc/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header-wrapper">
+    <h1 class="logo">
+      <BIconCompass class="icon" />
+      날씨앱
+      <small>v 0.1</small>
+    </h1>
+    <Nav />
+  </header>
+</template>
+
+<script>
+import { BIconCompass } from 'bootstrap-vue'
+import Nav from './Nav.vue'
+export default {
+  name: 'Header',
+  components: {
+    BIconCompass,
+    Nav
+  },
+  data () {
+    return {
+      items: [
+        { text: 'Daily', href: '/' },
+        { text: '5Days', href: '/5days' }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header-wrapper{
+  @include flex($h:space-between);
+  background-color: $dark-color;
+  color:$light-color;
+  font-size:1.5em;
+  .logo{
+    padding:.5em;
+  }
+  .icon {
+    font-size: .75em;
+  }
+}
+
+</style>
---
+++ src/components/inc/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li>
+      <router-link to="/">Daily</router-link>
+    </li>
+    <li>
+      <router-link to="/5days">5Days</router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<style lang="scss" scoped>
+ul{
+  @include flex;
+  li{
+    font-size:.75em;
+    cursor: pointer;
+    padding: .875em 1.5em;
+    margin: 0 .5em;
+    &:hover {
+      background-color: $secondary-color;
+      color:$dark-color;
+    }
+  }
+}
+</style>
