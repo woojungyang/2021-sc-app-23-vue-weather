e64719fee48ecb91eaae672d0a5c1f8fad9f5511
woojungyang <blossom1113@nate.com>
2021-10-13 10:55:02 +0900

    vuex 작업

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Daily from '../views/Daily.vue'
-import Days from '../views/Days.vue'
+// 일괄 롣딩
+// import Daily from '../views/Daily.vue'
+// import Days from '../views/Days.vue'
+
+// 지연로딩
+const Daily = () => import('../views/Daily.vue')
+const Days = () => import('../views/Days.vue')
 
 Vue.use(VueRouter)
 
